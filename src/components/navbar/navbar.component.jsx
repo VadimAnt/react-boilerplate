e68a6b50ff5a34d841608f301314820d63a8f61e
Vadim <webdevelopers52@gmail.com>
2018-08-26 09:57:17 +0300

    feat: added prettier

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -4,7 +4,9 @@ import './navbar.component.css';
 
 export default () => (
   <div>
-    <Link exact to="/">Home</Link>
+    <Link exact to="/">
+      Home
+    </Link>
     <Link to="/about-us">About</Link>
     <Link to="/about-us/test">About/test</Link>
     <Link to="/posts">Posts</Link>
