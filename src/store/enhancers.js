70a0142580561d80793ce4dff52547d3907f244d
Vadim <webdevelopers52@gmail.com>
2018-10-09 12:39:26 +0300

    feat: refactor

--- src/store/enhancers.js
+++ src/store/enhancers.js
@@ -1,5 +1,6 @@
 const enhancers = [];
 const env = process.env.REACT_APP_NODE_ENV || 'development';
+
 if (env === 'development' || env === 'staging') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
